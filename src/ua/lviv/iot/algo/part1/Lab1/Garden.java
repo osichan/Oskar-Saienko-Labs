ede941baf31c168959da0911ce6694ee7145d838
Oskar Saienko <oskarsajenko@gmail.com>
2023-03-13 13:16:39 +0200

    changed code by review

--- src/ua/lviv/iot/algo/part1/Lab1/Garden.java
+++ src/ua/lviv/iot/algo/part1/Lab1/Garden.java
@@ -1,4 +1,5 @@
 package ua.lviv.iot.algo.part1.Lab1;
+
 import lombok.*;
 
 @ToString
@@ -6,50 +7,39 @@ import lombok.*;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Garden {
     private double area;
     private boolean hasOrchard;
     private boolean hasVegetableGarden;
     private int numberOfFlowers;
-    private static Garden instance;
+    private static Garden instance = new Garden();
 
-    public void plantFlower(int count){
-        this.numberOfFlowers+= count;
+    public void plantFlower(int count) {
+        this.numberOfFlowers += count;
     }
-    public void removeFlower(int count){
-        if(count>numberOfFlowers){
-            this.numberOfFlowers=0;
-            System.out.println("There is no more flowers");
-        }else {
+
+    public void removeFlower(int count) {
+        if (count > numberOfFlowers) {
+            this.numberOfFlowers = 0;
+        } else {
             this.numberOfFlowers -= count;
         }
     }
-    public void addVegetableRegion(double area){
-        this.area+=area;
+
+    public void addVegetableRegion(double area) {
+        this.area += area;
     }
+
     public static Garden getInstance() {
-        if (instance == null) {
-            instance = new Garden();
-        }
         return instance;
     }
+
     public static void main(String[] args) {
 
-        Garden[] garden = new Garden[4];
-        garden[0] = new Garden();
-        garden[1] = new Garden(20.0,true,false,4);
-        garden[2] = Garden.getInstance();
-        garden[3] = Garden.getInstance();
+        Garden[] garden = {new Garden(),new Garden(20.0, true, false, 4),Garden.getInstance(),Garden.getInstance()};
 
-
-        garden[1].plantFlower(1);
-        garden[1].removeFlower(2);
-        garden[1].addVegetableRegion(20);
-        garden[2].plantFlower(2);
-        for(int i=0;i< garden.length;i++){
-            System.out.println(garden[i].toString());
+        for (Garden value : garden) {
+            System.out.println(value.toString());
         }
-
     }
 }
